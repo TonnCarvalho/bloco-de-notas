f3f2d5be45bc82061a9b941799c5b4635718169b
Cleiton <cleiton_601@hotmail.com>
2025-09-18 15:46:18 -0300

    feat: main rota e controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function() {
-    echo 'Aboult us';
-});
+Route::get('/main/{main}', [MainController::class, 'index']);
