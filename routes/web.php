5992a2f0bff330f8de6adba0d616867bbc7506cb
Cleiton <cleiton_601@hotmail.com>
2025-09-18 16:53:59 -0300

    feat: tela de login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
+// Auth
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
