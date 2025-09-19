117d9de92846ff0211adfa7d110b68af1f87e47b
Cleiton <cleiton_601@hotmail.com>
2025-09-19 15:22:42 -0300

    middleware isLogin

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function index()
+    {
+        echo 'Index';
+    }
 
+        public function novaNota()
+    {
+        echo 'novaNota';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,22 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\IsLogin;
 
 // Auth
 Route::get('/login', [AuthController::class, 'login'])
-->name('login');
+    ->name('login');
 Route::post('/login', [AuthController::class, 'submit'])
-->name('loginSubmit');
-Route::get('/logout', [AuthController::class, 'logout'])
-->name('logout');
+    ->name('loginSubmit');
+
+Route::middleware([IsLogin::class])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
+
+    Route::controller(MainController::class)->group(function () {
+        Route::get('/', 'index')
+            ->name('main');
+        Route::get('/nova-nota', 'novaNota')
+            ->name('novaNota');
+    });
+});
