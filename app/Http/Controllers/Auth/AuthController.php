d15f66f41c99f4d7359d7e95aa8cd4bf72e2634e
Cleiton <cleiton_601@hotmail.com>
2025-09-19 12:01:05 -0300

    fazendo login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -25,11 +26,45 @@ class AuthController extends Controller
                 'text_username' => 'required|string|min:3',
                 'text_password' => 'required'
             ],
-           //error messagem personalidade
+            //error messagem personalidade
             [
                 'required' => 'Campo requerido.',
                 'min' => 'Campo deve ter no minimo :min caracteres'
             ]
         );
+        //dados input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+
+        $user = User::where('username', $username)
+            ->where('deleted_at', NULL)
+            ->first();
+        //chegar se os dados existe
+        if (!$user) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with('loginError', 'Usuário ou senha incorreto');
+        }
+
+        if (!password_verify($password, $user->password)) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with('loginError', 'Usuário ou senha incorreto');
+        }
+        //update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        //login usuário
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->name
+            ]
+        ]);
+        echo 'OK';
     }
 }
