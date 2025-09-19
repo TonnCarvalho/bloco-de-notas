aed97f1354e9735693342d28d5acb10383b247f7
Cleiton <cleiton_601@hotmail.com>
2025-09-19 16:51:42 -0300

    buscando dados de usuario e notas

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
+        $id = session('user.id');
+
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+
         return view('home.home');
     }
 
-        public function novaNota()
+    public function novaNota()
     {
         echo 'novaNota';
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
-    //
+    public function notes(): HasMany
+    {
+        return $this->hasMany(Note::class);
+    }
 }
